5ae0e021ec30e8b1a3c730b8d2387d23cbabf44c
Ankit Verma <1133ankitverma@gmail.com>
2026-08-08 23:23:20 +0530

    Revert "revert git practice"
    This reverts commit 453dbd4f0636dc6bbe6bb916d0848b68a6f44eff.

--- Sales Data Analytics.sql
+++ Sales Data Analytics.sql
@@ -10,6 +10,6 @@ CREATE TABLE SalesData (
 -- Step 2: Insert initial data
 INSERT INTO SalesData (ProductName, Quantity, Price, SaleDate)
 VALUES
-
+('Tv', 5, 75000.00, '2026-08-01'),
 ('Smartphone', 10, 25000.00, '2026-08-02'),
 ('Headphones', 15,1000.00,"2026-08-09")
